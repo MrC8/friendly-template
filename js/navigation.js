e85fd3c638e1e4aa3ffe38b2168b698cc148d838
Boris <crik-8@hotmail.Fr>
2016-03-09 17:36:38 +0100

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -11,6 +11,11 @@
 	if ( ! container ) {
 		return;
 	}
+	
+	page = document.getElementById( 'page' );
+	if ( ! page ) {
+		return;
+	}	
 
 	button = container.getElementsByTagName( 'button' )[0];
 	if ( 'undefined' === typeof button ) {
@@ -29,22 +34,62 @@
 	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
 		menu.className += ' nav-menu';
 	}
-
+	
+	var sp1 = document.createElement("span");
+	sp1.setAttribute("id", "closebtn");
+	var sp1_content = document.createTextNode("Fermer");
+	sp1.appendChild(sp1_content);
+	var sp2 = document.getElementById("primary-menu");
+	var parentDiv = sp2.parentNode;
+	parentDiv.insertBefore(sp1, sp2);
+	
 	button.onclick = function() {
 		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
 			container.className = container.className.replace( ' toggled', '' );
+			page.className = page.className.replace( ' ouvert', '' );
 			button.setAttribute( 'aria-expanded', 'false' );
 			menu.setAttribute( 'aria-expanded', 'false' );
 		} else {
 			container.className += ' toggled';
+			page.className += ' ouvert';
 			button.setAttribute( 'aria-expanded', 'true' );
 			menu.setAttribute( 'aria-expanded', 'true' );
 		}
 	};
+	
+	sp1.onclick = function() {
+		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+			container.className = container.className.replace( ' toggled', '' );
+			page.className = page.className.replace( ' ouvert', '' );
+			menu.setAttribute( 'aria-expanded', 'false' );
+		} else {
+			container.className += ' toggled';
+			page.className += ' ouvert';
+			menu.setAttribute( 'aria-expanded', 'true' );
+		}
+	};		
 
 	// Get all the link elements within the menu.
 	links    = menu.getElementsByTagName( 'a' );
 	subMenus = menu.getElementsByTagName( 'ul' );
+	
+	
+	/////////////////////////////////////////////////////
+    var anchors = document.getElementById('menu-item-448'); //contact
+
+    anchors.onclick = function() {
+        if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+            container.className = container.className.replace( ' toggled', '' );
+            page.className = page.className.replace( ' ouvert', '' );
+            menu.setAttribute( 'aria-expanded', 'false' );
+        } else {
+            container.className += ' toggled';
+            page.className += ' ouvert';
+            menu.setAttribute( 'aria-expanded', 'true' );
+        }
+    };	
+	/////////////////////////////////////////////////////
+
 
 	// Set menu items with submenus to aria-haspopup="true".
 	for ( var i = 0, len = subMenus.length; i < len; i++ ) {
@@ -78,4 +123,5 @@
 			self = self.parentElement;
 		}
 	}
+	
 } )();
