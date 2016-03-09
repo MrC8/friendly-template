6f91849d6b0a6e5f10aef9a80b5803c3d248c296
Boris <crik-8@hotmail.Fr>
2016-03-09 17:32:23 +0100

    Update header.php

--- header.php
+++ header.php
@@ -21,9 +21,9 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site container">
+<div id="page" class="site">
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
+		<div class="site-branding container">
 			<?php if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
@@ -36,10 +36,14 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wiatheme' ); ?></button>
+		<a class="nav-btn" id="nav-open-btn" href="#nav"><i class="fa fa-bars"></i> <?php esc_html_e( 'Menu', 'revasso' ); ?>
+</a>
+		<nav id="nav" class="main-navigation">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+	
+    <?php if (is_home()) : ?>
+		<?php echo do_shortcode('[wonderplugin_slider id="1"]'); ?>
+	<?php endif; ?>
+    <div id="content" class="site-content container">
