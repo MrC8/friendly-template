5b7619667a5c7dc788caca43be6f9550676254f2
Boris <crik-8@hotmail.Fr>
2016-03-17 14:42:56 +0100

    Update content.php
    Add thumbnails and a readmore button to the blog articles

--- template-parts/content.php
+++ template-parts/content.php
@@ -21,19 +21,14 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'wiatheme' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
+	<?php if ( has_post_thumbnail()) : ?>
+            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="zethumb">
+        <?php the_post_thumbnail('medium', array('class' => 'alignleft')); ?>
+            </a>
+        <?php endif; ?>
 
 		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wiatheme' ),
-				'after'  => '</div>',
-			) );
+			the_content('<span class="zemore">Lire la suite</span>');
 		?>
 	</div><!-- .entry-content -->
 
