5bb1689c23082784d889d0a4e103f4251bc52fa8
Boris <crik-8@hotmail.Fr>
2017-03-08 11:15:59 +0100

    Update template-tags.php
    Ajout d'icon devant les meta dans le blog + ajout du nombre de commentaire sur 1 article

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -34,7 +34,23 @@ function wiatheme_posted_on() {
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	$comment_number = get_comments_number( $post_id );
+
+	if ( comments_open() ) {
+		if ( $comment_number == 0 ) {
+			$comments = __('No Comments');
+		} elseif ( $comment_number > 1 ) {
+			$comments = $num_comments . __(' Comments');
+		} else {
+			$comments = __('1 Comment');
+		}
+		$write_comments = ''. $comments.'';
+	} else {
+		$write_comments =  __('Comments are off for this post.');
+	}	
+
+	echo '<span class="posted-on"><i class="fa fa-calendar-o" aria-hidden="true"></i> ' . $posted_on . '</span> <span class="byline"><i class="fa fa-user-circle" aria-hidden="true"></i> ' . $byline . '</span> <span class="comments"><i class="fa fa-comment-o" aria-hidden="true"></i>
+ '. $write_comments .'</span>'; // WPCS: XSS OK.
 
 }
 endif;
